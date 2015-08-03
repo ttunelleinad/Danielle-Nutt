2702d9e460610cb191cdecebb9d4f6b291d2ec21
ttunelleinad <nutt.danielle@gmail.com>
2015-08-03 18:50:19 -0400

    single page site update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,77 @@
-//About Me Photo Swap
-$('img#danielle').mouseenter(function() {
-    $(this).css({'display':'none'});
-    $('img#danielle2').css({'display':'block'});
+//Single-Page Website
+$(function() {
+    $("nav a").click(function() {
+        var $linkClicked = $(this).attr("href");
+        document.location.hash = $linkClicked;
+        if (!$(this).hasClass("active")) {
+            $("nav a").removeClass("active");
+            $(this).addClass("active");
+            $("#container section").hide();
+            $($linkClicked).fadeIn();
+            return false;
+        }
+        else {
+            return false;
+        }
+    });
+    var hash = window.location.hash;
+    hash = hash.replace(/^#/, '');
+    switch (hash) {
+        case 'page2' :
+            $("#" + hash + "-link").trigger("click");
+            break;
+        case 'page3' :
+            $("#" + hash + "-link").trigger("click");
+            break;
+    }
 });
 
-$('img#danielle2').mouseleave(function() {
-    $(this).css({'display':'none'});
-    $('img#danielle').css({'display':'block'});
+//Show Homepage As Active Nav Item On Load
+$('nav a.page1').addClass('current');
+
+//Show Active Navigation Item
+$(function(){
+    $('nav a').click(function () {
+        $('nav a').removeClass('current');
+        $(this).addClass('current');
+     });
+ });
+
+//About Section Photo
+$('img#danielle').mouseenter(function() {
+    $(this).addClass("desaturate");
+});
+
+$('img#danielle').mouseleave(function() {
+    $(this).removeClass("desaturate");
+});
+
+//SlickNav
+$(function(){
+	$("#menu").slicknav();
+});
+
+//Form Validator
+$(function() {
+    $("form").submit(function(event){
+        event.preventDefault();
+        if(!$(".validator-error").length) {
+            var user = [$("#firstName").val(), $("#lastName").val(), $("#email").val(), $("#message").val()];
+            console.log(JSON.stringify(user));
+            $.ajax(
+                {
+                    type: "POST",
+                    url: "file://localhost/Users/daniellenutt/Documents/UArts/Web3/daniellenutt/index.html#page3",
+                    dataType: 'json',
+                    data: JSON.stringify(user),
+                    success: function() {
+                        console.log('complete');
+                    },
+                    error: function(){
+                        console.log("error");
+                    }
+                }
+            );
+        }
+    });
 });
